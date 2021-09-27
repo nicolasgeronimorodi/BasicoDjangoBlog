5cfedbf4aef09a2ea0653add1fbdaae4e03c1959
nicolasgeronimorodi <84360079+nicolasgeronimorodi@users.noreply.github.com>
2021-09-27 10:21:26 -0300

    Update settings.py

--- BasicoBlog/settings.py
+++ BasicoBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r4-8s$p5f-4_p*e@g(emi8ti!p7^$99+!0)z*4ntd*6&_!)_2c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
